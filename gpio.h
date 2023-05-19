38cfd13dad11bd6efcfd06ea9324d99c5193650b
Antonio Glenn <gl3nn0717@gmail.com>
2023-05-19 12:32:07 -0700

    Configured gpio pins to match schematic

--- gpio.h
+++ gpio.h
@@ -2,49 +2,46 @@
 #include "nrf_gpio.h"
 #include "nrf_drv_gpiote.h"
 
-// PDM
-// #define MIC_EN_PIN                       NRF_GPIO_PIN_MAP(0, 15)
-// #define PDM_CLK_PIN                      NRF_GPIO_PIN_MAP(1,8)
-// #define PDM_DATA_PIN                     NRF_GPIO_PIN_MAP(1,9)
 
-// SPI
-#define SPI_SCK_PIN                      NRF_GPIO_PIN_MAP(0,5)
-#define SPI_MOSI_PIN                     NRF_GPIO_PIN_MAP(0,6)
-#define SPI_MISO_PIN                     NRF_GPIO_PIN_MAP(0,8)
-#define SPI_CS_PIN                       NRF_GPIO_PIN_MAP(0,3)
+// SPI (IMU)
+#define SPI_SCK_PIN                       NRF_GPIO_PIN_MAP(0,5)
+#define SPI_MOSI_PIN                      NRF_GPIO_PIN_MAP(0,17)
+#define SPI_MISO_PIN                      NRF_GPIO_PIN_MAP(0,13)
+#define SPI_CS_PIN                        NRF_GPIO_PIN_MAP(0,3)
 
-// ACCEL
-#define ACCEL_EN_PIN                     NRF_GPIO_PIN_MAP(1, 4)
-#define ACCEL_INT1_PIN                   NRF_GPIO_PIN_MAP(1, 7)
-#define ACCEL_INT2_PIN                   NRF_GPIO_PIN_MAP(1, 6)
-
-// TRACE / UART
-#define GPIO_1_PIN                       NRF_GPIO_PIN_MAP(0, 5)
-// #define GPIO_2_PIN                       NRF_GPIO_PIN_MAP(0, 6)
-#define GPIO_3_PIN                       NRF_GPIO_PIN_MAP(0, 7)
-// #define GPIO_4_PIN                       NRF_GPIO_PIN_MAP(0, 8)
+// IMU
+#define ACCEL_INT1_PIN                    NRF_GPIO_PIN_MAP(0, 2)
+#define ACCEL_INT2_PIN                    NRF_GPIO_PIN_MAP(0, 31)
 
 // LED
-#define DEBUG_LED_PIN                    NRF_GPIO_PIN_MAP(0,12)                 // not functional on revA design
-#define BLE_LED_PIN                      NRF_GPIO_PIN_MAP(0,13)                 // 0 to turn on, 1 to turn off
+#define NRF_LED1_PIN                      NRF_GPIO_PIN_MAP(0,7)                 // not functional on revA design
+#define NRF_LED2_PIN                      NRF_GPIO_PIN_MAP(0,4)                 // 0 to turn on, 1 to turn off
+
+// Button
+#define NRF_BUTTON_PIN                    NRF_GPIO_PIN_MAP(0, 15)               
 
 // DEBUG_UART
-#define UART_TX_PIN                      NRF_GPIO_PIN_MAP(0, 6)
-#define UART_RX_PIN                      NRF_GPIO_PIN_MAP(0, 8)
+#define UART_TX_PIN                       NRF_GPIO_PIN_MAP(0, 6)
+#define UART_RX_PIN                       NRF_GPIO_PIN_MAP(0, 8)
 
 // I2C
-#define I2C_SCL_PIN                      NRF_GPIO_PIN_MAP(0, 27)
-#define I2C_SDA_PIN                      NRF_GPIO_PIN_MAP(0, 26)
+#define I2C_SCL_PIN                       NRF_GPIO_PIN_MAP(0, 27)
+#define I2C_SDA_PIN                       NRF_GPIO_PIN_MAP(0, 26)
 
 // QSPI
 // These are located in sdk_config.h. They're placed here just for reference.
-// #define NRFX_QSPI_PIN_SCK                NRF_GPIO_PIN_MAP(0, 19)
-// #define NRFX_QSPI_PIN_CSN                NRF_GPIO_PIN_MAP(0, 17)
-// #define NRFX_QSPI_PIN_IO0                NRF_GPIO_PIN_MAP(0, 20)
-// #define NRFX_QSPI_PIN_IO1                NRF_GPIO_PIN_MAP(0, 21)
-// #define NRFX_QSPI_PIN_IO2                NRF_GPIO_PIN_MAP(0, 22)
-// #define NRFX_QSPI_PIN_IO3                NRF_GPIO_PIN_MAP(0, 23)
-// #define FLASH_EN_PIN                     NRF_GPIO_PIN_MAP(0,16)
+#define NRFX_QSPI_PIN_SCK                NRF_GPIO_PIN_MAP(0, 19) // Camera PCLK
+#define NRFX_QSPI_PIN_CSN                NRF_GPIO_PIN_MAP(0, 29) // Frame Valid (FVLD)
+#define NRFX_QSPI_PIN_IO0                NRF_GPIO_PIN_MAP(0, 20) // D0 
+#define NRFX_QSPI_PIN_IO1                NRF_GPIO_PIN_MAP(0, 21) // D1
+#define NRFX_QSPI_PIN_IO2                NRF_GPIO_PIN_MAP(0, 22) // D2
+#define NRFX_QSPI_PIN_IO3                NRF_GPIO_PIN_MAP(0, 23) // D3
+
+// Camera 
+#define NRF_DVP_INT                      NRF_GPIO_PIN_MAP(0, 9) // INT 
+#define NRF_DVP_LVLD                     NRF_GPIO_PIN_MAP(0, 11) // Line Valid (LVLD)
+#define NRF_DVP_TRIG                     NRF_GPIO_PIN_MAP(0, 25) // Trigger (TRIG)
+#define NRF_DVP_MCLK                     NRF_GPIO_PIN_MAP(1, 8) // Master Clock (MCLK)
 
 // Wrapper
 #define GPIO_INTERRUPT_CONFIG_RISING  GPIOTE_CONFIG_IN_SENSE_LOTOHI(true)
