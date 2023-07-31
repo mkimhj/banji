ba710ddc045a7cd0e9be838aed56950c592b70b9
Antonio Glenn <gl3nn0717@gmail.com>
2023-07-31 13:24:27 -0700

    added MAX77650 library; Can set output voltages

--- main.c
+++ main.c
@@ -65,6 +65,7 @@ static bool streaming = false;
 static uint8_t metadataIndex = 0;
 static uint8_t metadata[180] = { 0 };
 
+
 void assert_nrf_callback(uint16_t line_num, const uint8_t * p_file_name)
 {
   app_error_handler(DEAD_BEEF, line_num, p_file_name);
